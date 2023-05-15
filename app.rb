c2b8e2e1dcc16af16823a8ee9ca5ff68124b0b64
roy kimathi <kimathiwaweru@gmail.com>
2023-05-16 00:33:00 +0300

    passed test

--- app.rb
+++ app.rb
@@ -1,23 +1,38 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
+# # pp [{ id: 1, hello: "World" }, 
+# #     { id: 2, hello: "Ruby" }, 
+# #     { id: 3, hello: "Moon" }, 
+# #     { id: 4, hello: "Learner" }]
 
-# p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+# # p [1,2,3]
+# # puts "Hello World!"
+# # print "Pass this test, please."
+# # p [1,2,3]
 
-# def add
+# # def add
     
+# # end
+# # # 1 + 1
+# # puts add + add
+
+# def add num1, num2
+#     puts num1
+#     puts num2
+#     num1 + num2
 # end
-# # 1 + 1
-# puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+# puts add(1,3) + add(2,2)
 
-puts add(1,3) + add(2,2)
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+# puts [1,2,3]
+# p [1, 2, 3]
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+puts "Hello World!"
+print "pass this test, please"
+p [1, 2 ,3]
